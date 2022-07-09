28154994f7fc536950de5600b3ecde1e4e723858
jaysmitjadhav <jadhav.jaysmit@gmail.com>
2022-07-09 14:13:15 -0700

    completed script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const li = document.querySelectorAll('li.dropdown a');
+const btn = document.querySelector('.nav-btn');
+const nav = document.querySelector('ul.navs');
+
+btn.addEventListener('click', e => {
+    nav.classList.toggle('toggle');
+})
+
+li.forEach((each) => {
+    if (each.nextElementSibling !== null) {
+        each.addEventListener('click', e => {
+            if (window.innerWidth < 768) {
+                e.target.parentElement.classList.toggle("active");
+            }
+        })
+    }
+})
